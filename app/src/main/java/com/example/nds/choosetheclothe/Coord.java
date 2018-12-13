072bd36617d25595df2118102af80ebf565df650
abrakadarar <neoname0q@gmail.com>
2018-12-13 17:45:51 +0300

    added reciving requests and showing data

--- app/src/main/java/com/example/nds/choosetheclothe/Coord.java
+++ app/src/main/java/com/example/nds/choosetheclothe/Coord.java
@@ -12,19 +12,15 @@ public class Coord {
     @SerializedName("lat")
     @Expose
     private Double lat;
-
     public Double getLon() {
         return lon;
     }
-
     public void setLon(Double lon) {
         this.lon = lon;
     }
-
     public Double getLat() {
         return lat;
     }
-
     public void setLat(Double lat) {
         this.lat = lat;
     }
